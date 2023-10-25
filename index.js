54ca7b1ab9b9028920c07fc31a3bae9e9e573dd6
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2023-10-25 19:13:03 +0530

    main code

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const randomcolor = function () {
     let color = "#";
     for (let i = 0; i < 6; i++) {
         color += hex[Math.floor(Math.random() * 16)];
-
     }
+    return color;
+};
+
+const startchangingcolor = function () { 
+    
+};
+
+document.querySelector("#start").addEventListener('click', startchangingcolor);
+
+document.querySelector("#Stop").addEventListener('click', stopchangingcolor);
+
+const stopchangingcolor = function () {
+
 };
